7bb06004380715128884aee573805111088e968b
Benoit Lemoine <blemoine@xebia.fr>
2013-05-25 13:10:13 +0200

    Correction server.js

--- server.js
+++ server.js
@@ -12,4 +12,3 @@ app.use(express.static(__dirname));
 
 app.listen(3000);
 console.log('go to http://localhost:3000/spaceinvaders.html to look at the game you\'re implementing');
-console.log('go to http://localhost:3000/spaceinvaders.test.html to look at the test your code must pass');
